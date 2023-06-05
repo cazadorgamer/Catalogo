713affd146ed1ac83bfa15c590da8242b7551b02
facundo m <118567052+cazadorgamer@users.noreply.github.com>
2023-06-05 02:30:53 +0200

    buscar beta

--- buscar.js
+++ buscar.js
@@ -1,9 +1,9 @@
 // Definir un array de objetos
 var bebida = [
-  { nombre: "cerveza palermo", precio: 460 },
-  { nombre: "lata sneider", precio: 300 },
-  { nombre: "lata gancia", precio: 360 },
-  { nombre: "manaos", precio: 290 }
+  { nombre: "cerveza palermo", precio: 460, Imagen: document.getElementById("imagen palermo") },
+  { nombre: "lata sneider", precio: 300,Imagen:document.getElementById("imagen palermo") },
+  { nombre: "lata gancia", precio: 360,Imagen:document.getElementById("imagen palermo") },
+  { nombre: "manaos", precio: 290,Imagen:document.getElementById("imagen palermo") }
 ];
 
 // Función de búsqueda y mostrar en HTML
@@ -24,9 +24,12 @@ function buscarPersona() {
  var resultadoContainer = document.getElementById("resultado-container");
  resultadoContainer.innerHTML = "";
 
+
  if (personaEncontrada) {
-   var resultadoHTML = "Nombre: " + personaEncontrada.nombre + "<br>Precio: " + personaEncontrada.precio;
+   var resultadoHTML = "Nombre: " + personaEncontrada.nombre + "<br>Precio: " + personaEncontrada.precio + "<br>";
    resultadoContainer.innerHTML = resultadoHTML;
+
+   
  } else {
    resultadoContainer.innerHTML = "No se encontró ninguna persona con ese nombre.";
  }
