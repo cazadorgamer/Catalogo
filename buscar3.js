ef272c4ff48ed899624093515249a15c2f41cf79
facundo m <118567052+cazadorgamer@users.noreply.github.com>
2023-09-15 17:46:36 -0300

    actualizacion de funciones

--- buscar3.js
+++ buscar3.js
@@ -4,16 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Simular una lista de productos con imágenes (puedes obtenerla de una fuente de datos real)
     const listaProductos = [
-        { nombre: 'cerveza Palermo', imagen: 'palermo.jpg' },
-        
-        { nombre: 'cerveza sneider', imagen: 'img/Cerveza-Schneider-Lata-473Ml-1-12957.webp' },
-        { nombre: 'cerveza anstel', imagen: 'img/anstel.jpg' },
-        { nombre: 'gaseosa manaos', imagen:'img/manaos.webp' },
-        
-        { nombre: 'gaseosa coca cola', imagen: 'img/coca vidrio.jpg' },
-        { nombre: 'latas gancia', imagen:  'img/latas gancia.webp' },
-        { nombre: 'latas sneider', imagen: 'img/Cerveza-Schneider-Lata-473Ml-1-12957.webp' },
-        { nombre: 'latas anstel', imagen: 'img/anstel.jpg' },
+        { nombre: 'cerveza palermo', imagenId: 'cerveza palermo' },
+        { nombre: 'coca vidrio', imagenId: 'coca vidrio' },
+        { nombre: 'manaos', imagenId: 'manaos' },
+        { nombre: 'lata sneider', imagenId: 'sneider' },
+        { nombre: 'lata gancia', imagenId: 'gancia' },
+        { nombre: 'lata anstel', imagenId: 'anstel' },
         // Agrega más productos aquí
     ];
 
@@ -27,25 +23,25 @@ document.addEventListener('DOMContentLoaded', () => {
             // El campo de búsqueda está vacío, no se muestran sugerencias
             return;
         }
-        const sugerenciasFiltradas = listaProductos.filter(producto => producto.nombre.toLowerCase().includes(textoBusqueda));
 
+        const sugerenciasFiltradas = listaProductos.filter(producto => producto.nombre.toLowerCase().includes(textoBusqueda));
 
         sugerenciasFiltradas.forEach(producto => {
             const listItem = document.createElement('li');
-            const imagen = document.createElement('img');
-            imagen.src = producto.imagen;
-            imagen.alt = producto.nombre;
             const nombre = document.createElement('span');
             nombre.textContent = producto.nombre;
 
-            // Agrega un manejador de eventos para desplazar la página cuando se hace clic en una sugerencia
+            // Agregar un manejador de eventos para desplazar la página a la imagen específica
             listItem.addEventListener('click', () => {
-                productosSection.scrollIntoView({ behavior: 'smooth' }); // Desplaza suavemente a la sección de productos
+                const imagen = document.getElementById(producto.imagenId);
+                if (imagen) {
+                    imagen.scrollIntoView({ behavior: 'smooth' }); // Desplazar suavemente hacia la imagen específica
+                }
             });
 
-            listItem.appendChild(imagen);
             listItem.appendChild(nombre);
             sugerencias.appendChild(listItem);
         });
     }
 });
+
