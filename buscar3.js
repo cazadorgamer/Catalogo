d2a3d31a0f9eaa08e18c1a4a896cca4e870f6add
facundo m <118567052+cazadorgamer@users.noreply.github.com>
2023-09-15 17:02:44 -0300

    actualizacion de funciones

---
+++ buscar3.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputBusqueda = document.getElementById('input-busqueda');
+    const sugerencias = document.getElementById('sugerencias');
+
+    // Simular una lista de productos con imágenes (puedes obtenerla de una fuente de datos real)
+    const listaProductos = [
+        { nombre: 'cerveza Palermo', imagen: 'palermo.jpg' },
+        
+        { nombre: 'cerveza sneider', imagen: 'img/Cerveza-Schneider-Lata-473Ml-1-12957.webp' },
+        { nombre: 'cerveza anstel', imagen: 'img/anstel.jpg' },
+        { nombre: 'gaseosa manaos', imagen:'img/manaos.webp' },
+        
+        { nombre: 'gaseosa coca cola', imagen: 'img/coca vidrio.jpg' },
+        { nombre: 'latas gancia', imagen:  'img/latas gancia.webp' },
+        { nombre: 'latas sneider', imagen: 'img/Cerveza-Schneider-Lata-473Ml-1-12957.webp' },
+        { nombre: 'latas anstel', imagen: 'img/anstel.jpg' },
+        // Agrega más productos aquí
+    ];
+
+    inputBusqueda.addEventListener('input', mostrarSugerencias);
+
+    function mostrarSugerencias() {
+        const textoBusqueda = inputBusqueda.value.toLowerCase();
+        sugerencias.innerHTML = '';
+
+        if (textoBusqueda === '') {
+            // El campo de búsqueda está vacío, no se muestran sugerencias
+            return;
+        }
+
+
+        const sugerenciasFiltradas = listaProductos.filter(producto => producto.nombre.toLowerCase().includes(textoBusqueda));
+
+        sugerenciasFiltradas.forEach(producto => {
+            const listItem = document.createElement('li');
+            const imagen = document.createElement('img');
+            imagen.src = producto.imagen;
+            imagen.alt = producto.nombre;
+            const nombre = document.createElement('span');
+            nombre.textContent = producto.nombre;
+            listItem.appendChild(imagen);
+            listItem.appendChild(nombre);
+            sugerencias.appendChild(listItem);
+        });
+    }
+});
--- carrito.js
+++ carrito.js
@@ -43,3 +43,61 @@ document.addEventListener('DOMContentLoaded', () => {
         totalElement.textContent = total.toFixed(2);
     }
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const agregarCarritoButtons = document.querySelectorAll('.agregar-carrito');
+    const listaCarrito = document.getElementById('lista-carrito');
+    const totalElement = document.getElementById('total');
+    let carrito = [];
+
+    agregarCarritoButtons.forEach(button => {
+        button.addEventListener('click', agregarAlCarrito);
+    });
+
+    listaCarrito.addEventListener('click', eliminarDelCarrito);
+
+    function agregarAlCarrito(event) {
+        const producto = event.target.parentElement;
+        const productoId = producto.getAttribute('data-id');
+        const productoNombre = producto.getAttribute('data-nombre');
+        const productoPrecio = parseFloat(producto.getAttribute('data-precio'));
+
+        const productoEnCarrito = carrito.find(item => item.id === productoId);
+
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad++;
+        } else {
+            carrito.push({
+                id: productoId,
+                nombre: productoNombre,
+                precio: productoPrecio,
+                cantidad: 1
+            });
+        }
+
+        mostrarCarrito();
+    }
+
+    function eliminarDelCarrito(event) {
+        if (event.target.classList.contains('eliminar-item')) {
+            const itemId = event.target.getAttribute('data-id');
+            carrito = carrito.filter(item => item.id !== itemId);
+            mostrarCarrito();
+        }
+    }
+
+    function mostrarCarrito() {
+        listaCarrito.innerHTML = '';
+
+        carrito.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                ${item.nombre} - Cantidad: ${item.cantidad} - Precio: $${item.precio * item.cantidad}
+                <button class="eliminar-item" data-id="${item.id}">Eliminar</button>
+            `;
+            listaCarrito.appendChild(listItem);
+        });
+
+        const total = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0);
+        totalElement.textContent = total.toFixed(2);
+    }
+});
