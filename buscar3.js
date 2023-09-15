da0726ebbdd3cf34a020974ce284d628f12cc745
facundo m <118567052+cazadorgamer@users.noreply.github.com>
2023-09-15 17:22:07 -0300

    actualizacion de funciones

--- buscar3.js
+++ buscar3.js
@@ -27,9 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // El campo de búsqueda está vacío, no se muestran sugerencias
             return;
         }
-
-
         const sugerenciasFiltradas = listaProductos.filter(producto => producto.nombre.toLowerCase().includes(textoBusqueda));
+
 
         sugerenciasFiltradas.forEach(producto => {
             const listItem = document.createElement('li');
@@ -38,6 +37,12 @@ document.addEventListener('DOMContentLoaded', () => {
             imagen.alt = producto.nombre;
             const nombre = document.createElement('span');
             nombre.textContent = producto.nombre;
+
+            // Agrega un manejador de eventos para desplazar la página cuando se hace clic en una sugerencia
+            listItem.addEventListener('click', () => {
+                productosSection.scrollIntoView({ behavior: 'smooth' }); // Desplaza suavemente a la sección de productos
+            });
+
             listItem.appendChild(imagen);
             listItem.appendChild(nombre);
             sugerencias.appendChild(listItem);
